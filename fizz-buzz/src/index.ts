f83a24056f4b7df475ceda209b3701f6722293de
Maksim <meison98@bk.ru>
2022-11-11 18:14:10 +0300

    feat: implemented fizzBuzz function

--- fizz-buzz/src/index.ts
+++ fizz-buzz/src/index.ts
@@ -1,4 +1,17 @@
 export const fizzBuzz = (_length: number) => {
-  // Place your code here.
-  return "FizzBuzz";
+  return Array.from(Array(_length).keys())
+    .map((i) => i + 1)
+    .map((i) => {
+      if (i % 3 === 0 && i % 5 === 0) {
+        return "FizzBuzz";
+      }
+      if (i % 3 === 0) {
+        return "Fizz";
+      }
+      if (i % 5 === 0) {
+        return "Buzz";
+      }
+      return i;
+    })
+    .join(",");
 };
